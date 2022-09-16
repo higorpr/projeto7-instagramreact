1840f0dd63d8a105c1b3f03f932861477a42ae72
higorpr <higorpr@gmail.com>
2022-09-16 17:03:37 -0300

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Body from "./Body";
+import BottomMobile from "./BottomMobile";
+import NavBar from "./NavBar";
+
+export default function App() {
+    return (
+        <div>
+            <NavBar />
+            <Body />
+            <BottomMobile/>
+        </div>
+    )
+}
