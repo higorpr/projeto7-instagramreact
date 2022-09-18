037d1c94b91313f2be066cbdda1b1c8be7d32817
higorpr <higorpr@gmail.com>
2022-09-18 04:11:23 -0300

    Revision B - Like and Bookmark functions completed

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,40 +1,68 @@
+import React from "react";
+
 function Post(props) {
+    const [iconHeart, setIconHeart] = React.useState('heart-outline');
+    const [iconColor, setIconColor] = React.useState('');
+    const [iconBookmark, setIconBookmark] = React.useState("bookmark-outline");
+    const [numLiked, setNumLiked] = React.useState(Number(props.nLiked));
+
+    function hitLike() {
+        if (iconHeart === 'heart-outline') {
+            setIconHeart('heart');
+            setIconColor('red');
+            setNumLiked(numLiked + 1);
+
+        } else {
+            setIconHeart('heart-outline')
+            setIconColor('');
+            setNumLiked(numLiked - 1);
+        }
+    }
+
+    function hitBookmark() {
+        if (iconBookmark === 'bookmark-outline') {
+            setIconBookmark('bookmark');
+        } else {
+            setIconBookmark('bookmark-outline')
+        }
+    }
+
     return (
         <il class="post">
-        <div class="topo">
-            <div class="usuario">
-                <img src={props.userIcon} alt={`user_${props.userName}`}/>
-                {props.userName}
-            </div>
-            <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-            </div>
-        </div>
-
-        <div class="conteudo">
-            <img src={props.postImage} />
-        </div>
-
-        <div class="fundo">
-            <div class="acoes">
-                <div>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <ion-icon name="chatbubble-outline"></ion-icon>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
+            <div class="topo">
+                <div class="usuario">
+                    <img src={props.userIcon} alt={`user_${props.userName}`} />
+                    {props.userName}
                 </div>
-                <div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
+                <div class="acoes">
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
                 </div>
             </div>
 
-            <div class="curtidas">
-                <img src={props.likedIcon} />
-                <div class="texto">
-                    Curtido por <strong>{props.likedBy}</strong> e <strong>outras {props.nLiked} pessoas</strong>
+            <div class="conteudo">
+                <img src={props.postImage} onClick={hitLike} />
+            </div>
+
+            <div class="fundo">
+                <div class="acoes">
+                    <div>
+                        <ion-icon name={iconHeart} onClick={hitLike} class={iconColor}></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
+                    </div>
+                    <div>
+                        <ion-icon name={iconBookmark} onClick={hitBookmark}></ion-icon>
+                    </div>
+                </div>
+
+                <div class="curtidas">
+                    <img src={props.likedIcon} />
+                    <div class="texto">
+                        Curtido por <strong>{props.likedBy}</strong> e <strong>outras {numLiked.toLocaleString().replace(',','.')} pessoas</strong>
+                    </div>
                 </div>
             </div>
-        </div>
-    </il>
+        </il>
     )
 }
 export default function Posts() {
@@ -45,7 +73,7 @@ export default function Posts() {
             postImage: "./assets/img/gato-telefone.svg",
             likedBy: "respondeai",
             likedIcon: "./assets/img/respondeai.svg",
-            nLiked: '101.523'
+            nLiked: '101523'
         },
         {
             userName: 'barked',
@@ -53,19 +81,19 @@ export default function Posts() {
             postImage: "./assets/img/dog.svg",
             likedBy: "adorable_animals",
             likedIcon: "./assets/img/adorable_animals.svg",
-            nLiked: '99.159'
+            nLiked: '99159'
         },
     ];
-    
+
     return (
         <ul class="posts">
-            {postContent.map((e) => <Post 
-            userName={e.userName}
-            userIcon={e.userIcon}
-            postImage={e.postImage}
-            likedBy={e.likedBy}
-            likedIcon={e.likedIcon}
-            nLiked={e.nLiked}/>)}
+            {postContent.map((e) => <Post
+                userName={e.userName}
+                userIcon={e.userIcon}
+                postImage={e.postImage}
+                likedBy={e.likedBy}
+                likedIcon={e.likedIcon}
+                nLiked={e.nLiked} />)}
         </ul>
     )
 
